f33aa0b878889c7e9371bcfac7c58faf0379070e
Quan Dinh Vu <vudinhquan124@gmail.com>
2016-11-07 14:24:05 +0000

    Starting from the end of Practical 1

--- search/Node.java
+++ search/Node.java
@@ -4,9 +4,14 @@ public class Node {
 	public final Node parent;
 	public final Action action;
 	public final State state;
+	public final int depth;
 	
 	public Node(Node parent, Action action, State state) {
 		this.parent = parent;
+		if(this.parent == null)
+			depth = 0;
+		else
+			depth = parent.depth + 1;
 		this.action = action;
 		this.state = state;
 	}
